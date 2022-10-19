b848eb8ddc72ae5f50c38c8148c1f88c7a33c19c
“Marcela <mpmontiela@gmail.com>
2022-10-19 14:31:59 +0200

    game over and score

--- js/index.js
+++ js/index.js
@@ -13,6 +13,12 @@ cat.src = "../images/cat.png"
 const ghost = new Image(); 
 ghost.src = "../images/ghost.png"; 
 
+//SCORE VARIABLES
+
+let score = document.querySelector("timer");
+let timer; 
+
+
 //MOVING STUFF
 let isGameOver = false;
 let gameId = 0;
@@ -22,10 +28,28 @@ let ghostX = 700;
 let ghostY = 300;
 const catHeight = -200;
 const catWidth = 200;
+const ghostHeight = 100;
+const ghostWidth = 200;
 let isMovingRight = false; // ARROW KEYS
 let isMovingLeft = false;  // ARROW KEYS
 let isMovingUp = false;   // ARROW KEYS
 let isMovingDown = false; // ARROW KEYS
+let monsters = [{img: ghost, x: ghostX, y: ghostY, height: ghostHeight, width: ghostWidth},
+  {img: ghost, x: ghostX+700, y: ghostY, height: ghostHeight, width: ghostWidth},
+  {img: ghost, x: ghostX+400, y: ghostY, height: ghostHeight, width: ghostWidth},
+  {img: ghost, x: ghostX+300, y: ghostY, height: ghostHeight, width: ghostWidth}]; 
+
+// FUNCTION SCORE
+
+function timeIsRunnning() {
+  var sec = 0;
+  var timer = setInterval(function(){
+      document.getElementById('timer').innerHTML='00:'+sec;
+      sec++;
+      if(count == 59) clearInterval(timer);
+     }, 1000);
+}
+
 
 //GHOST
 
@@ -58,9 +82,6 @@ function updateCanvas() {
   requestAnimationFrame(updateCanvas);
 }
 
-// start calling updateCanvas once the image is loaded
-ghost.onload = updateCanvas;
-
 
 //START BUTTON AND HIDE CANVAS 
 window.onload = () => {
@@ -71,46 +92,56 @@ window.onload = () => {
     console.log("starting");
   canvas.style.display = "block"; // THIS IS THE BACKGROUND
     startGame();}
+    timeIsRunnning()
   document.getElementById("end-button").onclick = () => {
-      console.log("game over");
+     console.log("game over");
      isGameOver = false;
      gameId = 0;
      canvas.style.display = "block";
      gameOver.style.display = "none";
     startGame();}  
   
-  
 
-
-  function startGame() {
+function startGame() {
     console.log(gameId);
     startScreen.style.display = "none"; 
     gameBoard.style.display = "flex"; 
     ctx.drawImage(background, 0, 0, canvas.width , canvas.height); // THIS IS THE SIZE FROM BACKGROUND
-    ctx.drawImage(cat, catX, catY, catWidth, catHeight);   // THIS IS THE SIZE FROM THE CAT
-    ctx.drawImage(ghost, ghostX, ghostY, 200, 100); 
-    ghostX -=2
+    ctx.drawImage(cat, catX, catY, catWidth, catHeight);   // THIS IS THE SIZE FROM THE CAT 
+// GAME OVER 
 
-
-if(isGameOver) { 
+   //function endGame () {
+    if(isGameOver == true) { 
     cancelAnimationFrame(gameId);
     gameOver.style.display = "flex"
     console.log("game over");
     canvas.style.display = "none";
     }  
-  else {
-  gameId = requestAnimationFrame(startGame); 
-}
-
-if (gameId > 100) {
-  isGameOver = true;}
+    else {
+    gameId = requestAnimationFrame(startGame); 
+    }
+   
+  // COLLISION
   
-  
-
-
+    for (let i= 0; i< monsters.length; i++) {
+      let currentMonster = monsters[i] 
+      ctx.drawImage(currentMonster.img, currentMonster.x, currentMonster.y, currentMonster.width, currentMonster.height);
+      currentMonster.x -=3
+      if(currentMonster.y > canvas.height){
+        currentDog.y = -500;
+      }
+      if(currentMonster.y + currentMonster.width > catY &&
+         catX + catWidth > currentMonster.x &&
+         catX < currentMonster.x + currentMonster.width &&
+         catY + currentMonster.height > currentMonster.y
+         ){
+          isGameOver = true;
+         }   
+        } 
+   
 
  // MOVE THE CAT
- 
+  
  if (isMovingRight === true) {
   catX += 2
 } else if (isMovingLeft === true) {
